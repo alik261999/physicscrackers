9bcaa126c62d12952dfc1a3ee05d2e36b9b030d0
alik261999 <87687342+alik261999@users.noreply.github.com>
2021-07-21 16:31:29 +0530

    Update info.js

--- dist/info.js
+++ dist/info.js
@@ -1,16 +1,5 @@
 let points=window.sessionStorage.getItem("points");
 console.log(points);
 window.onload=function(){
-    fire();
     document.querySelector("div.result").innerHTML="Your Obtained Marks is "+points;
-}
-
-
-function fire(){
-
-    db.collection("202107A").add({
-      Name: namee,
-      email_ID: mail,
-      Marks: points
-    });
 }
